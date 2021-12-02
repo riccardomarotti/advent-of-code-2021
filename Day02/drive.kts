6c4d5e11f2b26c694a14cd8a304dc35879abbc87
Riccardo <riccardo.marotti@memooria.org>
2021-12-02 17:57:38 +0100

    Add day 2 solution

---
+++ Day02/drive.kts
@@ -0,0 +1,9 @@
+val lines = java.io.File("input").readLines()
+fun getValue(it: String) = it.split(" ")[1].toInt()
+
+val horizontal = lines.filter { it.startsWith("forward") }.map { getValue(it) }.reduce { acc, i -> acc+i }
+var depth = lines.filter { it.startsWith("down") }.map { getValue(it) }.reduce { acc, i -> acc+i }
+depth -= lines.filter { it.startsWith("up") }.map { getValue(it) }.reduce { acc, i -> acc+i }
+
+println(depth*horizontal)
+
